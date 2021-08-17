53083d15643630cf442e92f617b3d37bfed18f3b
sinagun <38187655+sinagun@users.noreply.github.com>
2021-08-17 13:08:06 +0430

    0.1

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+pcnumber=random.randint(0,2)
+print(pcnumber)
+userguss=input(print("guss the number"))
+
+
+if pcnumber==userguss:
+    print("nice")
+
+else:print("your worng")
+
