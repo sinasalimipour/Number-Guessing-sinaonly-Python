ba7290c91e1bc8bd28c8ff81aff913858507d035
sinagun <38187655+sinagun@users.noreply.github.com>
2021-08-17 13:28:50 +0430

    0.5

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 
 
-userguss = int(input("guss the number"))
-pcnumber = random.randint(0, 0)
+userguss = int(input("guss the number between 0 and 2 you got 1 change"))
+pcnumber = random.randint(0, 2)
 if (userguss == pcnumber):
     print("nice")
-else:print("worng")
+else:print("wrong")
